eb1a2822e29911f7f36325feb1243d3a48c30609
Julien Kirch <code@archiloque.net>
2019-05-17 20:22:50 +0200

    no message

--- tiles-generator/src/main/java/net/archiloque/babaisyousolver/tilesgenerator/Main.java
+++ tiles-generator/src/main/java/net/archiloque/babaisyousolver/tilesgenerator/Main.java
@@ -23,20 +23,29 @@ import java.util.List;
  */
 public class Main {
 
-  public static final String TILES_JSON_FILES = "tiles.json";
+  private static final String TILES_JSON_FILES = "tiles.json";
 
   public static void main(String[] args)
       throws Exception {
     if (args.length != 1) {
-      throw new IllegalArgumentException("Need one argument and got " + args.length);
+      throw new IllegalArgumentException(
+          "Need one argument and got " + args.length);
     }
     String targetDir = args[0];
     System.out.println("Will generate in [" + targetDir + "]");
-    URI jsonResourceUri = Main.class.getClassLoader().getResource(TILES_JSON_FILES).toURI();
-    String tilesFileContent = Files.readString(Path.of(jsonResourceUri));
+
+    // get the JSON content
+    URI jsonResourceUri = Main.
+        class.
+        getClassLoader().
+        getResource(TILES_JSON_FILES).
+        toURI();
+    String tilesFileContent = Files.
+        readString(Path.of(jsonResourceUri));
     JSONObject jsonObject = new JSONObject(tilesFileContent);
     System.out.println(jsonObject.length() + " tiles found");
 
+    // create a list with all values, being sure empty is first
     List<String> fields = new ArrayList<>();
     for (String fieldName : jsonObject.keySet()) {
       if (!fieldName.equals("empty")) {
@@ -46,26 +55,41 @@ public class Main {
     fields.sort(String::compareToIgnoreCase);
     fields.add(0, "empty");
 
+    // initialize the interface
     TypeSpec.Builder tileInterface = TypeSpec.
         interfaceBuilder("Tiles")
         .addModifiers(Modifier.PUBLIC).
             addAnnotation(
                 AnnotationSpec.builder(
                     Generated.class).
-                    addMember("value", "$S", Main.class.getName()).
+                    addMember(
+                        "value",
+                        "$S",
+                        Main.class.getName()).
                     build());
 
     List<String> fieldsNames = new ArrayList<>();
 
+    // declare the XXX_STRING String constants
     for (String field : fields) {
-      String fieldConstantName = field.toUpperCase().replace(' ', '_');
+      // create the constant from the field name
+      String fieldConstantName =
+          field.
+              toUpperCase()
+              .replace(' ', '_');
       fieldsNames.add(fieldConstantName);
       FieldSpec tileField = FieldSpec.
-          builder(String.class, fieldConstantName + "_STRING", Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL).
+          builder(
+              String.class,
+              fieldConstantName + "_STRING",
+              Modifier.PUBLIC,
+              Modifier.STATIC,
+              Modifier.FINAL).
           initializer("$S", field).build();
       tileInterface.addField(tileField);
     }
 
+    // declare the ALL_STRINGS containing the XXX_STRING constants
     CodeBlock.Builder allStringCode = CodeBlock.
         builder().
         add(" new String[]{\n");
@@ -74,33 +98,52 @@ public class Main {
     }
     allStringCode.
         add("}");
-
     FieldSpec allStringsField = FieldSpec.
-        builder(ArrayTypeName.of(String.class), "ALL_STRINGS", Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL).
-        initializer(allStringCode.build()).build();
-
+        builder(
+            ArrayTypeName.of(String.class),
+            "ALL_STRINGS",
+            Modifier.PUBLIC,
+            Modifier.STATIC,
+            Modifier.FINAL).
+        initializer(allStringCode.build()).
+        build();
     tileInterface.addField(allStringsField);
 
+    // the int values
     for (int i = 0; i < fieldsNames.size(); i++) {
       String fieldName = fieldsNames.get(i);
       FieldSpec valueField = FieldSpec.
-          builder(TypeName.INT, fieldName, Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL).
+          builder(
+              TypeName.INT,
+              fieldName,
+              Modifier.PUBLIC,
+              Modifier.STATIC,
+              Modifier.FINAL).
           initializer("" + i).build();
       tileInterface.addField(valueField);
     }
 
+    // the masks
     for (int i = 1; i < fieldsNames.size(); i++) {
       String fieldName = fieldsNames.get(i);
       FieldSpec maskField = FieldSpec.
-          builder(TypeName.INT, fieldName + "_MASK", Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL).
+          builder(
+              TypeName.INT,
+              fieldName + "_MASK",
+              Modifier.PUBLIC,
+              Modifier.STATIC,
+              Modifier.FINAL).
           initializer("1 << " + (i - 1)).build();
       tileInterface.addField(maskField);
     }
 
-    JavaFile javaFile = JavaFile.builder("net.archiloque.babaisyousolver", tileInterface.build())
+    // create the file
+    JavaFile javaFile =
+        JavaFile.builder(
+            "net.archiloque.babaisyousolver",
+            tileInterface.build())
         .build();
     javaFile.writeTo(new File(targetDir));
-
   }
 
 }
